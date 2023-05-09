0eb313f87fedba2cc0ef7fc6204199e3bfc1ea1a
ClaudiaSando <al248156@edu.uaa.mx>
2023-05-09 11:45:06 -0600

    menu
    dfghjklñ

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { DomseguroPipe } from './domseguro.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DomseguroPipe
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
